64bd9e59b35ddf0fb68c0aa88462d0393f624f87
KatoPachi <vge008kh@student.econ.osaka-u.ac.jp>
2021-08-09 10:42:50 +0900

    ggtempの更新

--- R/ggtemp.r
+++ R/ggtemp.r
@@ -2,6 +2,10 @@
 #'
 #'
 #' @param flip logical value to whether `ggplot2::coord_flip()` is implemented.
+#' @param family specify font registered in `grDevices::windowsFonts()`
+#' @param size list of font size. `title` is axis.title and legend.title.
+#'   `text` is axis.text and legend.text. `caption` is plot.caption.
+#' @param legend_key_size specify legend.key.size. Unit is "cm."
 #'
 #' @importFrom ggplot2 theme_minimal
 #' @importFrom ggplot2 theme
@@ -14,22 +18,33 @@
 #'
 #' @export
 #+
-ggtemp <- function(flip = FALSE) {
+ggtemp <- function(flip = FALSE,
+                   family = NULL,
+                   size = list(
+                     title = 13,
+                     text = 9,
+                     caption = 11
+                   ),
+                   legend_key_size = 1) {
 
-  my_theme <- theme_minimal() +
+  my_theme <- theme_minimal(base_family = family) +
     theme(
       panel.grid.major.x = element_blank(),
       panel.grid.minor.x = element_blank(),
       panel.grid.minor.y = element_blank(),
-      axis.text = element_text(color = "black", size = 13),
-      axis.title = element_text(size = 13),
+      axis.text = element_text(
+        color = "black", size = size$text, family = family
+      ),
+      axis.title = element_text(size = size$title, family = family),
       axis.ticks.length = unit(0.25, "cm"),
       axis.ticks.x = element_line(),
       axis.ticks.y = element_line(),
       axis.line = element_line(),
-      legend.text = element_text(size = 13),
-      legend.key.size = unit(1, "cm"),
-      legend.position = "bottom"
+      legend.text = element_text(size = size$text, family = family),
+      legend.key.size = unit(legend_key_size, "cm"),
+      legend.title = ggplot2::element_text(size = size$title),
+      legend.position = "bottom",
+      plot.caption = ggplot2::element_text(size = size$caption)
     )
 
   if (flip) {
